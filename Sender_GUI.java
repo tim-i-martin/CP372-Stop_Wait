d97dd9a924474d08694dcd3847e20285f379311a
tim-i-martin <i.tim.martin@gmail.com>
2021-10-25 10:10:48 -0400

    preliminary GUI commit

---
+++ Sender_GUI.java
@@ -0,0 +1,356 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerListModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JTextArea;
+import java.awt.Color;
+
+public class Sender_GUI {
+	//private BoardClient client = null;
+	private JFrame frmClientPortal;
+	private JTextField txtIPReceiver;
+    private JTextField txtPORTReceiver;
+    private JTextField txtPORTSender;
+	private JTextField txtFILEInput;
+	private JTextField txtTimeout;
+
+	private JTextField textRefersTo;
+
+	// Swing objects that will get passed to the BoardClient class
+	private JTextArea textDisplay;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sender_GUI window = new Sender_GUI();
+					window.frmClientPortal.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Sender_GUI() {
+		initialize();
+	}
+
+	/**
+	 * Create frmClientPortal JFrame
+	 */
+	private void renderClientPortal() {
+		frmClientPortal = new JFrame();
+		frmClientPortal.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frmClientPortal.setBackground(new Color(102, 255, 51));
+		frmClientPortal.setTitle("Sending Portal");
+		frmClientPortal.setBounds(100, 100, 652, 594);
+		frmClientPortal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frmClientPortal.getContentPane().setLayout(gridBagLayout);
+	};
+
+	/**
+	 * Create txtIPaddress text field for Receiver
+	 */
+	private void renderTextIPReceiver() {
+		txtIPReceiver = new JTextField();
+		txtIPReceiver.setToolTipText("enter IP address of Receiver in format xxx.x.x.x");
+		txtIPReceiver.setText("IP Receiver");
+		GridBagConstraints gbc_txtIPReceiver = new GridBagConstraints();
+        gbc_txtIPReceiver.gridwidth = 3;
+        gbc_txtIPReceiver.insets = new Insets(0, 0, 5, 5);
+        gbc_txtIPReceiver.fill = GridBagConstraints.HORIZONTAL;
+        gbc_txtIPReceiver.gridx = 3;
+        gbc_txtIPReceiver.gridy = 2;
+		frmClientPortal.getContentPane().add(txtIPReceiver, gbc_txtIPReceiver);
+		txtIPReceiver.setColumns(10);
+	}
+
+	/**
+	 * create txtPORTReceiver to identify the receiver PORT number
+	 */
+	private void renderTextPORTReceiver() {
+		txtPORTReceiver = new JTextField();
+		txtPORTReceiver.setToolTipText("enter the 4 digit port number");
+		txtPORTReceiver.setText("PORT Receiver");
+		GridBagConstraints gbc_txtPORTReceiver = new GridBagConstraints();
+		gbc_txtPORTReceiver.gridwidth = 3;
+		gbc_txtPORTReceiver.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPORTReceiver.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPORTReceiver.gridx = 3;
+		gbc_txtPORTReceiver.gridy = 4;
+		frmClientPortal.getContentPane().add(txtPORTReceiver, gbc_txtPORTReceiver);
+		txtPORTReceiver.setColumns(10);
+
+	}
+
+	/**
+	 * create txtPORTSender JtextField() to identify where to receive ACK's
+	 */
+	private void renderTextPORTSender() {
+		txtPORTSender = new JTextField();
+		txtPORTSender.setToolTipText("enter the PORT to receive ACK's");
+		txtPORTSender.setText("PORT SENDER");
+		GridBagConstraints gbc_txtPORTSender = new GridBagConstraints();
+		gbc_txtPORTSender.gridwidth = 3;
+		gbc_txtPORTSender.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPORTSender.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPORTSender.gridx = 3;
+		gbc_txtPORTSender.gridy = 5;
+		frmClientPortal.getContentPane().add(txtPORTSender, gbc_txtPORTSender);
+		txtPORTSender.setColumns(10);
+	
+	}
+
+	/**
+	 * create txtFILEInput JtextField() as a field to capture the file name to send
+	 */
+	private void renderTextFILEInput() {
+		txtFILEInput = new JTextField();
+		txtFILEInput.setToolTipText("enter the file name to transmit");
+		txtFILEInput.setText("filepath");
+		GridBagConstraints gbc_txtFILEInput = new GridBagConstraints();
+		gbc_txtFILEInput.gridwidth = 3;
+		gbc_txtFILEInput.insets = new Insets(0, 0, 5, 5);
+		gbc_txtFILEInput.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtFILEInput.gridx = 3;
+		gbc_txtFILEInput.gridy = 6;
+		frmClientPortal.getContentPane().add(txtFILEInput, gbc_txtFILEInput);
+		txtFILEInput.setColumns(10);
+	}
+
+	/**
+	 * create TextTimeout JtextField() as a field to capture the timeout calc
+	 */
+	private void renderTextTimeout() {
+		txtTimeout = new JTextField();
+		txtTimeout.setToolTipText("enter the timeout in ms");
+		txtTimeout.setText("timeout");
+		GridBagConstraints gbc_txtTimeout = new GridBagConstraints();
+		gbc_txtTimeout.gridwidth = 3;
+		gbc_txtTimeout.insets = new Insets(0, 0, 5, 5);
+		gbc_txtTimeout.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtTimeout.gridx = 3;
+		gbc_txtTimeout.gridy = 7;
+		frmClientPortal.getContentPane().add(txtTimeout, gbc_txtTimeout);
+		txtTimeout.setColumns(10);
+	}
+
+	private void renderBtnALIVE() {
+		JButton btnISALIVE = new JButton("ISALIVE");
+		GridBagConstraints gbc_btnISALIVE = new GridBagConstraints();
+		gbc_btnISALIVE.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnISALIVE.insets = new Insets(0, 0, 5, 5);
+		gbc_btnISALIVE.gridx = 12;
+		gbc_btnISALIVE.gridy = 2;
+		frmClientPortal.getContentPane().add(btnISALIVE, gbc_btnISALIVE);
+		actionBtnISALIVE(btnISALIVE);
+
+	}
+
+	/**
+	 * create IPReceiverLabel JLabel()
+	 */
+	private void renderIPReceiverLabel() {
+		JLabel IPReceiverLabel = new JLabel("IP address of Receiver");
+		GridBagConstraints gbc_IPReceiverLabel = new GridBagConstraints();
+		gbc_IPReceiverLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_IPReceiverLabel.gridx = 1;
+		gbc_IPReceiverLabel.gridy = 2;
+		frmClientPortal.getContentPane().add(IPReceiverLabel, gbc_IPReceiverLabel);
+	}
+
+	/**
+	 * create PORTReceiverLabel JLabel()
+	 */
+	private void renderPORTReceiverLabel() {
+		JLabel PORTReceiverLabel = new JLabel("Receiver's PORT#");
+		GridBagConstraints gbc_PORTReceiver = new GridBagConstraints();
+		gbc_PORTReceiver.insets = new Insets(0, 0, 5, 5);
+		gbc_PORTReceiver.gridx = 1;
+		gbc_PORTReceiver.gridy = 4;
+		frmClientPortal.getContentPane().add(PORTReceiverLabel, gbc_PORTReceiver);
+	}
+
+	/**
+	 * create PORTSenderLabel JLabel()
+	 */
+	private void renderPORTSenderLabel() {
+		JLabel PORTSenderLabel = new JLabel("Senders's PORT# for ACK");
+		GridBagConstraints gbc_PORTSender = new GridBagConstraints();
+		gbc_PORTSender.insets = new Insets(0, 0, 5, 5);
+		gbc_PORTSender.gridx = 1;
+		gbc_PORTSender.gridy = 5;
+		frmClientPortal.getContentPane().add(PORTSenderLabel, gbc_PORTSender);
+	}
+
+
+    /**
+	 * create lblFILEInput JLabel()
+	 */
+	private void renderLblFILEInput() {
+		JLabel lblFILEInput = new JLabel("File Name to Send");
+		GridBagConstraints gbc_lblFILEInput = new GridBagConstraints();
+		gbc_lblFILEInput.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFILEInput.gridx = 1;
+		gbc_lblFILEInput.gridy = 6;
+		frmClientPortal.getContentPane().add(lblFILEInput, gbc_lblFILEInput);
+	}
+
+	/**
+	 * create TimeoutLabel JLabel()
+	 */
+	private void renderTimeoutLabel() {
+		JLabel TimeoutLabel = new JLabel("Enter timeout length (ms): ");
+		GridBagConstraints gbc_TimeoutLabel = new GridBagConstraints();
+		gbc_TimeoutLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_TimeoutLabel.gridx = 1;
+		gbc_TimeoutLabel.gridy = 7;
+		frmClientPortal.getContentPane().add(TimeoutLabel, gbc_TimeoutLabel);
+	}
+
+	/**
+	 * Create btnGET JButton
+	 */
+	private JButton renderBtnSEND() {
+		JButton btnSEND = new JButton("SEND");
+		actionBtnSEND(btnSEND);
+		GridBagConstraints gbc_btnSEND = new GridBagConstraints();
+		gbc_btnSEND.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSEND.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSEND.gridx = 12;
+		gbc_btnSEND.gridy = 4;
+		frmClientPortal.getContentPane().add(btnSEND, gbc_btnSEND);
+
+		return btnSEND;
+	}
+
+
+	/**
+	 *
+	 */
+	private void renderTextDisplay() {
+		textDisplay = new JTextArea();
+		GridBagConstraints gbc_textDisplay = new GridBagConstraints();
+		gbc_textDisplay.gridwidth = 12;
+		gbc_textDisplay.insets = new Insets(0, 0, 0, 5);
+		gbc_textDisplay.fill = GridBagConstraints.BOTH;
+		gbc_textDisplay.gridx = 1;
+		gbc_textDisplay.gridy = 12;
+		frmClientPortal.getContentPane().add(textDisplay, gbc_textDisplay);
+		textDisplay.setEditable(false);
+	}
+
+	/**
+	 * adds Action listener to CONNECT buttion
+	 */
+	private void actionBtnALIVE(JButton btnConnect) {
+		btnConnect.addActionListener(e -> {
+			String IP = txtIPReceiver.getText();
+			try {
+				int port = Integer.parseInt(txtPORTReceiver.getText());
+			} catch (Exception ex) {
+				textDisplay.setText("Invalid Request");
+			}
+		});
+	}
+
+	/**
+	 * adds Action listener to GET buttion
+	 */
+	private void actionBtnSEND(JButton btnSEND) {
+		btnSEND.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+			
+				String refersTo = textRefersTo.getText();
+				
+				//client.GET(colour, x, y, refersTo);
+
+				} catch (Exception ex){
+					textDisplay.setText("Invalid Request");
+				}
+
+			}
+		});
+	}
+
+	/**
+	 * adds Action listener to GET buttion
+	 */
+	private void actionBtnISALIVE(JButton btnISALIVE) {
+		btnISALIVE.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+                    textDisplay.setText("tried to test alive");
+
+				} catch (Exception ex){
+					textDisplay.setText("Invalid Request");
+				}
+
+			}
+		});
+	}
+
+
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		// render Encapsulating frame widget
+		renderClientPortal();
+
+		// render input boxes for IP address() and PORT
+
+        renderTextIPReceiver();
+        renderIPReceiverLabel();
+
+        renderTextPORTReceiver();
+        renderPORTReceiverLabel();
+
+        renderTextPORTSender();
+        renderPORTSenderLabel();
+
+        renderTextFILEInput();
+        renderLblFILEInput();
+
+        renderTextTimeout();
+        renderTimeoutLabel(); 
+
+        renderBtnALIVE();
+		renderBtnSEND();
+
+
+
+
+		//Renders the Text Display for information from Server
+		renderTextDisplay();
+	}
+
+}
